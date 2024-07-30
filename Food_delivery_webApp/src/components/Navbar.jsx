d3e838faabed92c0cf61d893e573060d11ee9807
pankaj-gitcode <pankajaditya75@gmail.com>
2024-07-30 23:14:38 +0530

    +Navbar_CSS-Active +pages_dir_added

--- Food_delivery_webApp/src/components/Navbar.jsx
+++ Food_delivery_webApp/src/components/Navbar.jsx
@@ -14,15 +14,17 @@ export default function Navbar(){
                 <ul className="flex gap-4 items-center justify-between font-[ubuntu] text-[#49557e] text-[18px]">
                     {
                         menuItems.map((elem)=>
-                        <li key='1' className="hover:underline cursor-pointer">{elem.item}</li>
+                        <li key='menuNav' className=" active:border-b-red-950 active:border-b-2  cursor-pointer">{elem.item}</li>
                         )
                     }
                 </ul>
 
                 <div className='flex items-center justify-between gap-6'>
                     <img src={assets.search_icon} alt="search" />
-                    <div>
+                        {/* ----- Cart ----- */}
+                    <div className='relative'>
                         <img src={assets.basket_icon} alt="cart" />
+                        <div className='absolute w-2 h-2 bg-red-600 -top-1 -right-1 rounded-full hover:bg-slate-50'></div>
                     </div>
                     <button className="border-2 rounded-[300px] border-orange-600 px-6 py-1 text-[#49557e] text-[16px] bg-transparent">SignIn</button>
                 </div>
