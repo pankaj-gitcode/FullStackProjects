c1736cae9a3923651d1f0c4784409aa213eea78e
pankaj-gitcode <pankajaditya75@gmail.com>
2024-08-26 17:06:33 +0530

    +cart items added

--- Food_delivery_webApp/src/components/pages/Cart/Cart.jsx
+++ Food_delivery_webApp/src/components/pages/Cart/Cart.jsx
@@ -1,19 +1,44 @@
 import React from 'react'
+import { useRecoilValue } from 'recoil'
+import { countItemsAtom, foodItemsAtom } from '../../atom'
 
 export default function Cart(){
+    const foodItem = useRecoilValue(foodItemsAtom());
+    const countItems = useRecoilValue(countItemsAtom);
+    console.log("cont: ", countItems) 
+    
     return(<>
 
-        <div>
-            <div>
-                <div>
-                    <p>Image</p>
-                    <p>Name</p>
-                    <p>Cost</p>
-                    <p>Quantity</p>
-                    <p>Total</p>
-                    <p>Remove</p>
+        <div className="py-20">
+            <div className="">
+                    <div className="grid grid-cols-6 items-center justify-between">
+                        <p>Items</p>
+                        <p>Name</p>
+                        <p>Cost</p>
+                        <p>Quantity</p>
+                        <p>Total</p>
+                        <p>Remove</p>
                 </div>
-                <hr />
+                    <div className="">
+                    {
+                        foodItem.map((elem,index)=>{
+                            {/* check if elem._id is the id in object countItems */}
+                            if(countItems[elem._id] > 0){return(
+                                <div key={index} className="grid grid-cols-6 items-center justify-between">
+                                <img src={elem.image} alt="" className="size-20 object-contain "/>
+                                <p>{elem.name}</p>
+                                <p>{elem.price}</p>
+                                <p>{countItems[elem._id]}</p>
+                                <p>{elem.price * countItems[elem._id] }</p>
+                                <p>x</p>
+                                <hr className="w-[76vw]"/>
+                                </div>)
+                            }
+                        })
+                    }
+                    </div>
+                
+                <br />
             </div>
         </div>
 
