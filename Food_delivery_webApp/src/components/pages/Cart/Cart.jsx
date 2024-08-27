031f5c879369abe0c0ec9382820885fa3b2881e8
pankaj-gitcode <pankajaditya75@gmail.com>
2024-08-27 19:21:56 +0530

    cart page added some more functionality

--- Food_delivery_webApp/src/components/pages/Cart/Cart.jsx
+++ Food_delivery_webApp/src/components/pages/Cart/Cart.jsx
@@ -8,7 +8,14 @@ export default function Cart(){
     const [countItems, setCountItems] = useRecoilState(countItemsAtom);
    
     console.log("cont: ", countItems)
-    const clickHandle = (id)=>{
+    const clickHandleAdd = (id)=>{
+        setCountItems((prevCount)=>{
+            const currentCount = {...prevCount,[id]:(prevCount[id] || 0) + 1}
+            return currentCount;
+        })
+    }
+
+    const clickHandleSub = (id)=>{
         setCountItems((prevCount)=>{
             const newCount = {...prevCount,[id]:Math.max(prevCount[id]-1, 0)};
             // delete newCount[id];
@@ -20,7 +27,7 @@ export default function Cart(){
 
         <div className="py-20">
             <div className="">
-                    <div className="grid grid-cols-6 items-center justify-between">
+                    <div className="grid grid-cols-6 items-center justify-between font-semibold">
                         <p>Items</p>
                         <p>Name</p>
                         <p>Cost</p>
@@ -28,18 +35,26 @@ export default function Cart(){
                         <p>Total</p>
                         <p>Remove</p>
                 </div>
-                    <div className="">
+                    <div className="pt-6">
                     {
                         foodItem.map((elem,index)=>{
                             {/* check if elem._id is the id in object countItems */}
                             if(countItems[elem._id] > 0){return(
-                                <div key={index} className="grid grid-cols-6 items-center justify-between">
+                                <div key={index} className="grid grid-cols-6 items-center justify-between text-sm gap-5 lg:gap-0">
                                 <img src={elem.image} alt="" className="size-20 object-contain "/>
                                 <p>{elem.name}</p>
                                 <p>{elem.price}</p>
                                 <p>{countItems[elem._id]}</p>
                                 <p>{elem.price * countItems[elem._id] }</p>
-                                <p onClick={()=>clickHandle(elem._id)}>x</p>
+                                {/* ------------- add/remove items ------------ */}
+                                <p className=" border-2 w-fit flex">
+                                    <span 
+                                    className="p-2 cursor-pointer text-green-500 font-bold text-xl"
+                                     onClick={()=>clickHandleAdd(elem._id)}>+</span>
+                                    <span 
+                                    className="p-2 cursor-pointer text-red-500 font-bold text-xl"
+                                     onClick={()=>clickHandleSub(elem._id)}> -</span>
+                                </p>
                                 <hr className="w-[76vw]"/>
                                 </div>)
                             }
@@ -48,6 +63,34 @@ export default function Cart(){
                     </div>
                 
                 <br />
+
+                    {/* -------------- Total | Promocode -------------- */}
+                    <div className="grid grid-cols-2 gap-10 mt-10">
+
+                        {/* --------------  total | checkout button --------- */}
+                        <div>
+                            <h1 className="font-bold py-2">Cart Total</h1>
+                            <div className="grid grid-cols-2 text-sm w-[50vw]">
+                                <p>Subtotal <hr /></p>
+                                <p>0</p>
+                                <p>Delivery Fee <hr /></p>
+                                <p>2</p>
+                                <p className="font-bold">Total</p>
+                                <p className="font-bold">0</p>
+                            </div >
+                                <button className="bg-orange-600 text-[#fff] px-2 py-1 lg:px-4 lg:py-1 rounded-sm text-xs lg:text-sm active:scale-105 mt-2">PROCEED TO CHECKOUT</button>
+                           
+                        </div>
+                        {/* -------- PROMOCODE ----------- */}
+                        <div className="">
+                            <p className="text-sm font-semibold">Have a Promocode</p>
+                            <div>
+                                <input type="text" placeholder="Enter Promocode" className="bg-slate-200 text-sm pl-2 pr-20 py-1 rounded-l-sm focus:outline-none"/>
+                                <button className="bg-[#000] text-[#fff] text-sm py-1 px-5 rounded-r-sm">Submit</button>
+                            </div>
+                        </div>
+                    </div>
+
             </div>
         </div>
 
