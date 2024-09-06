4877626de014b7b4d9de9f9ac7bc1a78e4b38b05
pankaj-gitcode <pankajaditya75@gmail.com>
2024-09-06 13:45:01 +0530

    admin initiated

---
+++ admin/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+export default function App(){
+
+  return(<>
+    <div>
+      <h1 className="text-xl font-bold">HEELOO</h1>
+
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum, velit dolor! Voluptas repudiandae in cumque porro aspernatur ab iure deleniti vero dicta, nisi dolorem molestiae commodi accusamus qui aliquam consectetur eius necessitatibus!</p>
+    </div>
+  </>)
+}
