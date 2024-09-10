22c50e2d05757bac43e3486165637e45e577e1ea
pankaj-gitcode <pankajaditya75@gmail.com>
2024-09-10 09:20:16 +0530

    +addItems_inputs_successfully_pushed_from_UI_to_DB

--- admin/src/components/Page/Add/Add.jsx
+++ admin/src/components/Page/Add/Add.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { assets } from '../../../assets/assets';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { dataAtom } from '../../Atom';
+import axios from 'axios';
 
 const Add = ()=>{
     const [upload, setUpload] = useState(false);
@@ -15,12 +16,35 @@ const Add = ()=>{
         }))
     }
 
+const submitHandler = async (e)=>{
+    const URL = 'http://localhost:3000'
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("name",data.name);
+    formData.append("description", data.description);
+    formData.append("category", data.category);
+    formData.append("price", Number(data.price));
+    formData.append("image", upload);
+
+    const response = await axios.post(`${URL}/api/food/add`, formData);
+
+    if(response.data.success){
+        setData({
+            name: '',
+            description:'',
+            category: '',
+            price:''
+        })
+        setUpload(false);
+    }
+}
+
     console.log("DATA: ", data);
     return(<>
         <div className="flex p-12 ">
             <div>
 
-                <form className="text-lg text-[#787777]">
+                <form className="text-lg text-[#787777]" onSubmit={submitHandler}>
 
                     {/* --------- Upload Image ---------- */}
                     <div className="flex flex-col gap-2 ">
@@ -35,13 +59,13 @@ const Add = ()=>{
                     {/* --------- Product name ---------- */}
                     <div className="my-6">
                         <p className="my-2">Product name</p>
-                        <input type="text" className="border-2 border-solid w-[30vw] p-1 text-sm focus:outline-none" placeholder="Type here" name="name" onChange={clickHandler}/>
+                        <input type="text" className="border-2 border-solid w-[30vw] p-1 text-sm focus:outline-none" placeholder="Type here" name="name"value={data.name} onChange={clickHandler}/>
                     </div>
 
                     {/* --------- Product description ---------- */}
                     <div>
                         <p className="my-2">Product description</p>
-                        <textarea name="description" rows='5' placeholder="write some content here" className="border-2 pl-1 w-[30vw] text-sm focus:outline-none" onChange={clickHandler}></textarea>
+                        <textarea name="description" value={data.description} rows='5' placeholder="write some content here" className="border-2 pl-1 w-[30vw] text-sm focus:outline-none" onChange={clickHandler} required></textarea>
                     </div>
 
                     {/* --------- Product category & price ---------- */}
@@ -49,7 +73,7 @@ const Add = ()=>{
                             {/* ----- category ------ */}
                         <div>
                             <p>Product Category</p>
-                            <select name="category" className="text-sm text-[#000]" onChange={clickHandler}>
+                            <select name="category" value={data.category} className="text-sm text-[#000]" onChange={clickHandler}>
                                 <option value="Salad">Salad</option>
                                 <option value="Rolls">Rolls</option>
                                 <option value="Sandwhich">Sandwhich</option>
@@ -63,13 +87,13 @@ const Add = ()=>{
                             {/* ------- Price -------- */}
                         <div>
                             <p>Product Price</p>
-                            <input type="number" name="price" onChange={clickHandler} placeholder="$12" className="focus:outline-none border-2 w-[5vw]" />
+                            <input type="number" name="price" value={data.price} onChange={clickHandler} placeholder="$12" className="focus:outline-none border-2 w-[5vw]" />
                         </div>
                     </div>
 
                     {/* -------------- Add button ------------ */}
                     <div className="pt-5">
-                        <button className="bg-[#000] py-2 px-10 text-[#fff] text-sm active:bg-[#fff] active:border-2 active:border-slate-900 active:text-[#000] shadow-[1px_2px_5px_1px_rgba(0,0,0,0.5)] rounded-sm active:scale-90 duration-300 ">ADD</button>
+                        <button className="bg-[#000] py-2 px-10 text-[#fff] text-sm active:bg-[#fff] active:border-2 active:border-slate-900 active:text-[#000] shadow-[1px_2px_5px_1px_rgba(0,0,0,0.5)] rounded-sm active:scale-90 duration-300 " id='image'>ADD</button>
                     </div>
                 </form>
 
