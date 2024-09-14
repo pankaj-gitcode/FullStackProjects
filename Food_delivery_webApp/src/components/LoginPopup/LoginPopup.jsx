3f68eb8f6f6d1d86e36ba151ca1ec7efd98a78e5
pankaj-gitcode <pankajaditya75@gmail.com>
2024-09-14 16:43:23 +0530

    signup_login_card_updated

--- Food_delivery_webApp/src/components/LoginPopup/LoginPopup.jsx
+++ Food_delivery_webApp/src/components/LoginPopup/LoginPopup.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useRecoilState, useRecoilValue } from 'recoil'
 import { allIconsAtom, dataAtom, loginAtom } from '../atom';
+import axios from 'axios';
 
 export default function LoginPopup(){
     const exit = useRecoilValue(allIconsAtom);
@@ -9,70 +10,52 @@ export default function LoginPopup(){
     const [data, setData] = useRecoilState(dataAtom);
 
 
-    // inputHandler: to update the local DB/Object
-    const onChangeHandler = (event)=>{
-        const name = event.target.name;
-        const value = event.target.value;
-
-        setData(data=>({
-            ...data, [name]:value
-        }))
-    }
-
     return(<>
-    <div>
+        <div>
+            <form >
+                <div className='w-fit bg-slate-200'>
 
-        <form className="bg-[#fff] flex flex-col place-items-between px-10 py-2 w-fit rounded-lg overflow-hidden shadow-[2px_2px_6px_2px_rgba(0,0,0,0.6)]">
-
-                {/* ---------- page title + Exit ----------- */}
-                <div className="flex justify-between items-center pb-5 cursor-pointer active">
-                    <h1 className="font-bold text-2xl md:text-3xl lg:text-2xl">{currentState}</h1>
-                    <img src={exit.cross_icon} alt="exit" className="w-3 h-3 " onClick={()=>setLogin(false)}/>
-                </div>
-                {/* ---------------------- SignUp -------------------- */}
-            {               
-                currentState==="SignUp"?<div >
-
-            <div className="grid gap-3 pt-2">
-                {/* --------- form: name,email,pass-------- */}
-                <input type="text" placeholder="Enter name" className="border-[2px] border-slate-200 text-2xl lg:text-sm rounded-md focus:outline-none pl-2" required/>
-                <input type="email" placeholder="Enter mail" className="border-[2px] border-slate-200 text-2xl lg:text-sm rounded-md focus:outline-none pl-2" required/>
-                <input type="password" placeholder="Enter password" className="border-[2px] border-slate-200 text-2xl lg:text-sm rounded-md focus:outline-none pl-2" required/>
-
-                {/* ----------- create accnt. button ----------- */}
-                <div>
-                    <p className="text-2xl lg:text-sm bg-orange-600 rounded-sm text-center cursor-pointer active:scale-105 transition-[300] ease-in-out py-1">Create account</p>
-                </div>
-            </div>
-                    {/* ---------- checkbox ---------- */}
-            <div className="flex flex-col place-items-start py-6 gap-2 text-xl lg:text-sm">
-                <input type="checkbox" name="" id=""/>
-                            {/* ------- T&C ------- */}
-                <p className="">By Continuing, I agree to the terms of use & privacy policy</p>
-                            {/* ------- change to 'Login'--------- */}
-                <p>Already have an account? <span onClick={()=>setCurrentState("Login")} className="cursor-pointer text-orange-600 font-semibold">Login here</span></p>
-            </div>
-
-                </div>:<div>
-                    {/* ------------- login credentials ---------- */}
-                    <div className="flex flex-col gap-2 pb-6 pt-2">
-
-                        <input type="email" placeholder="Enter Email" className="border-[2px] border-slate-200 text-2xl lg:text-sm rounded-md focus:outline-none pl-2" required/>
-
-                        <input type="password" placeholder="Enter password" className="border-[2px] border-slate-200 text-2xl lg:text-sm rounded-md focus:outline-none pl-2" required/>
-
-                        <div className="pt-2">
-                            <p className="text-2xl lg:text-sm bg-orange-600 rounded-sm text-center cursor-pointer active:scale-105 transition-[300] ease-in-out py-1">Submit</p>
-                        </div>
+                    {/* ----------- close icon ---------- */}
+                    <div>
+                        <h1>{currentState}</h1>
+                        <img src={exit.cross_icon} alt="close_button" onClick={()=>setLogin(false)} className="cursor-pointer  "/>
                     </div>
 
-                     {/* ------------- already have account ---------------- */}
-                     <p className="pb-5">Click here to <span onClick={()=>setCurrentState("SignUp")} className="cursor-pointer text-orange-600 font-semibold ">SignUp</span></p>
+                    {/* ------------- inputs: name,email, password ------------- */}
+                    <div>
+                        { currentState==='SignUp'?<input type='text' placeholder='Enter name' required/>:<></> }
+                        <input type="email" placeholder='Enter email' required/>
+                        <input type="password" placeholder='Enter password' required/>
+                    </div>
+
+                    {/* ---------------- buttons:Signup/Login ----------------- */}
+                    <div>
+                        <button>
+                            {
+                                currentState === 'SignUp'?<span>Submit</span>:<span>Login</span>
+                            }
+                        </button>
+                    </div>
+
+
+                    {/* --------------- check box -------------- */}
+                    <div>
+                        <input type="checkbox" required/>
+                        <p>By continuing, I agree the terms of use & privacy policy.</p>
+                    </div>
+
+                    {/* ---------------- flip Login-Signup ------------ */}
+                    <div>
+                        {
+                            currentState==='SignUp'?
+                            <p>Already have an account? <span className='text-orange-600 cursor-pointer' onClick={()=>setCurrentState('Login')}>Login here</span> </p>
+                            :
+                            <p>Create a new account? <span className='text-orange-600 cursor-pointer' onClick={()=>setCurrentState('SignUp')}>SignUp</span></p>
+                        }
+                    </div>
                 </div>
-            }
 
-        </form>
-
-    </div>
+            </form>
+        </div>
     </>)
 }
